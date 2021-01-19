534cc26ceeb3eb9f6e6de482693cdc0eb1b89b83
Ravinder Negi <r.singh.negi85@gmail.com>
2021-01-19 18:31:44 +0530

    Updated

--- App.js
+++ App.js
@@ -4,32 +4,41 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#2ecc71',
     flex: 1,
+    flexDirection: 'row',
+  },
+  main: {
+    /* justifyContent: 'center', flex: 1  */
   },
   block: {
     /* flex: 1,
     justifyContent: 'center',
     alignContent: 'space-around',
-	flexWrap: 'wrap',*/
+	flexWrap: 'wrap',
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
+
+    ,*/
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'space-around',
+  },
+  button: {
+    backgroundColor: 'blue',
+    alignContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    shadowColor: '#ddd',
+    shadowOffset: 3,
+    shadowOpacity: 1,
   },
 });
 function App() {
   return (
     <View style={styles.container}>
       <View style={styles.block}>
-        <View
-          style={{
-            backgroundColor: 'blue',
-            alignContent: 'center',
-            alignItems: 'center',
-            borderRadius: 8,
-            shadowColor: '#ddd',
-            shadowOffset: 3,
-            shadowOpacity: 1,
-          }}
-        >
+        <View style={styles.button}>
           <Text
             style={{
               textAlign: 'center',
@@ -37,7 +46,32 @@ function App() {
               margin: 30,
             }}
           >
-            Button
+            1
+          </Text>
+        </View>
+      </View>
+      <View style={styles.block}>
+        <View style={styles.button}>
+          <Text
+            style={{
+              color: '#fff',
+              margin: 30,
+            }}
+          >
+            2
+          </Text>
+        </View>
+      </View>
+      <View style={styles.block}>
+        <View style={styles.button}>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: '#fff',
+              margin: 30,
+            }}
+          >
+            3
           </Text>
         </View>
       </View>
